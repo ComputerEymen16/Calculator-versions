8b0d2107d6c21025b383328ca957d401c97d66aa
Computer_Eymen <eymenetka1616@gmail.com>
2026-04-19 20:06:16 +0300

    Add files via upload

---
+++ Calculator 1.0v/Calculator 1.0v.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+int main() {
+	float number1, number2;
+	char operation;
+	std::cout << " Calculator 1.0v by Computer Eymen\n";
+	std::cout << "Enter first number: ";
+	std::cin >> number1;
+	std::cout << "Enter second number: ";
+	std::cin >> number2;
+	std::cout << "Enter operation (+, -, *, /): ";
+	std::cin >> operation;
+
+	if (operation == '+') {
+		std::cout << "Result: " << number1 + number2;
+	}
+
+	if (operation == '-') {
+		std::cout << "Result: " << number1 - number2;
+	}
+
+	if (operation == '*') {
+		std::cout << "Result: " << number1 * number2;
+	}
+
+	if (operation == '/') {
+		std::cout << "Result: " << number1 / number2;
+	}
+
+	getchar(); // Wait for user input before closing the console
+	std::cout << "\nCalculator 1.0v by Computer Eymen\n";
+    std::cout << "Press Enter to close...";
+	getchar();
+	return 0;
+}
